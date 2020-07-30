09e92fc851dfec8fb11a6b8422ffa0a1f57c68a3
marcos pinardi <exegeses@gmail.com>
2020-07-30 12:41:06 -0300

    variables y constantes en PHP

--- variables.php
+++ variables.php
@@ -1,1 +1,15 @@
 <?php
+    $numero = 25;
+    echo 'El número es: ', $numero;
+?>
+<br>
+<?php
+    #constantes
+    const NOMBRE = 'Marcos';
+    const APELLIDO = 'Pinardi';
+    echo 'Tu nombre es: ', NOMBRE, ' ', APELLIDO;
+?>
+<br>
+<?php
+    echo 'El número es: ', $numero;
+?>
