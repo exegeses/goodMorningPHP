ecffc3e33ca9c44129613231d3fcd6cf229e6cb1
marcos pinardi <exegeses@gmail.com>
2020-08-04 10:21:50 -0300

    resolución tarea condicionales

---
+++ clase2/condicionales.php
@@ -0,0 +1,45 @@
+<?php
+
+    $numero = 150;
+
+    if ( $numero < 100 ){
+        echo '<img src="imagenes/ok.png">';
+    }
+    else {
+        echo '<img src="imagenes/error.png">';
+    }
+?>
+<hr>
+<?php
+    $numero = 150;
+    if( $numero < 100 ){
+?>
+    <img src="imagenes/ok.png">
+<?php
+    }
+    else{
+?>
+    <img src="imagenes/error.png">
+<?php
+    }
+?>
+<hr>
+<?php
+    $numero = 150;
+    if( $numero < 100 ){
+        $img = 'ok';
+    }
+    else{
+        $img = 'error';
+    }
+?>
+    <img src="imagenes/<?php echo $img ?>.png">
+<hr>
+<?php
+    $numero = 150;
+    $img = 'error';
+    if( $numero < 100 ){
+        $img = 'ok';
+    }
+?>
+<img src="imagenes/<?php echo $img ?>.png">
