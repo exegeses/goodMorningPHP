e71acca2e3a4e214cb9af9b41ed732dfa92d252d
marcos pinardi <exegeses@gmail.com>
2020-08-06 12:50:16 -0300

    ejercicio muestra de colores

--- clase3/muestraColores.php
+++ clase3/muestraColores.php
@@ -1,3 +1,49 @@
 <?php
 
-    
+    $colores = [
+                    'azul', 'rojo', 'violeta',
+                    'verde', 'amarillo', 'rosa',
+                    'naranja','marron'
+                ];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        body{
+            font-family: Helvetica;
+            font-size: 1em;
+            color: #555;
+            width: 700px;
+            margin: auto;
+        }
+        #muestras{
+            border: 1px solid #999;
+            padding: 16px 0px 32px 16px;
+        }
+            #muestras span{
+                display: inline-block;
+                position: relative;
+                top: 11px;
+                width: 32px;
+                height: 32px;
+                margin-right: 12px;
+                border: 1px solid #999;
+            }
+
+    </style>
+</head>
+<body>
+    <h1>Muestra de colores</h1>
+    <div id="muestras">
+        
+        <span style="background-color: #ccc;"></span>
+            nombre color
+        <br>
+        
+    </div>
+
+</body>
+</html>
