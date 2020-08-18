ecececbdbe99e43cc71021f74dfc8bde8cb5894f
marcos pinardi <exegeses@gmail.com>
2020-08-18 13:06:20 -0300

    Panel de administración de productos

--- sistema/adminProductos.php
+++ sistema/adminProductos.php
@@ -37,8 +37,8 @@
                 <tr>
                     <td><?= $producto['prdNombre'] ?></td>
                     <td><?= $producto['prdPrecio'] ?></td>
-                    <td><?= $producto['idMarca'] ?></td>
-                    <td><?= $producto['idCategoria'] ?></td>
+                    <td><?= $producto['mkNombre'] ?></td>
+                    <td><?= $producto['catNombre'] ?></td>
                     <td><?= $producto['prdPresentacion'] ?></td>
                     <td>
                         <img src="productos/<?= $producto['prdImagen'] ?>" class="img-thumbnail">
