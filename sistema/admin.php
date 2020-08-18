d4a2841cace42083076bf0981d8a01998d259a57
marcos pinardi <exegeses@gmail.com>
2020-08-18 10:40:56 -0300

    catalogo de productos

---
+++ sistema/admin.php
@@ -0,0 +1,27 @@
+<?php  
+	
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Dashboard - Panel principal</h1>
+
+        <section class="list-group">
+            <a href="adminMarcas.php" class="list-group-item list-group-item-action">
+                Panel de administración de marcas.
+            </a>
+            <a href="adminCategorias.php" class="list-group-item list-group-item-action">
+                Panel de administración de Categorías.
+            </a>
+            <a href="adminProductos.php" class="list-group-item list-group-item-action">
+                Panel de administración de Productos.
+            </a>
+            <a href="adminUsuarios.php" class="list-group-item list-group-item-action">
+                Panel de administración de Usuarios.
+            </a>
+        </section>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
