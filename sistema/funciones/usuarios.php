edb54f5652331688ca09655aaed35fae37e720cd
marcos pinardi <exegeses@gmail.com>
2020-08-20 12:55:10 -0300

    panel de administraciónde usuarios

--- sistema/funciones/usuarios.php
+++ sistema/funciones/usuarios.php
@@ -3,6 +3,16 @@
     ###########################
     ##### CRUD DE USUARIOS #####
 
+    function listarUsuarios()
+    {
+        $link = conectar();
+        $sql = "SELECT idUsuario, usuNombre, usuApellido, usuEmail
+                            FROM usuarios
+                            WHERE usuEstado = 1";
+        $resultado = mysqli_query($link, $sql) or die( mysqli_error($link) );
+        return $resultado;
+    }
+
     /*
      * listarUsuarios()
      * verUsuarioProID()
