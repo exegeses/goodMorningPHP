a2a562893e80282fd63cc1fa9002c1697e3e65ef
marcos pinardi <exegeses@gmail.com>
2020-08-25 12:52:17 -0300

    aplicación para agregar un producto

---
+++ sistema/agregarProducto.php
@@ -0,0 +1,31 @@
+<?php  
+
+    //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+        $chequeo = agregarProducto();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Alta de un nuevo producto</h1>
+<?php
+        $class = 'danger';
+        $mensaje = 'No se pudo agregar el producto';
+        if( $chequeo ) {
+            $class = 'success';
+            $mensaje = 'Producto agregado correctamente';
+        }
+?>
+        <div class="alert alert-<?= $class ?>">
+            <?= $mensaje ?>
+            <br>
+            <a href="adminProductos.php" class="btn btn-outline-secondary">
+                volver a panel
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
