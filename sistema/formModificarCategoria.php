f77564bc8114cd016c8c6be99716eb7998d9bdba
marcos pinardi <exegeses@gmail.com>
2020-08-27 10:17:30 -0300

    formulario para modificar categorías

---
+++ sistema/formModificarCategoria.php
@@ -0,0 +1,24 @@
+<?php  
+	
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Alta de una nueva marca</h1>
+
+        <div class="bg-light border p-4">
+            <form action="agregarMarca.php" method="post">
+                Marca: <br>
+                <input type="text" name="mkNombre" class="form-control">
+                <br>
+                <button class="btn btn-dark">Agregar marca</button>
+                <a href="adminMarcas.php" class="btn btn-outline-secondary">
+                    volver a panel
+                </a>
+            </form>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
