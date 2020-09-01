8d04a755ffbdb66df4da1732e82abe595ee8b63d
marcos pinardi <exegeses@gmail.com>
2020-09-01 11:37:36 -0300

    informe de modificación de producto

--- sistema/modificarProducto.php
+++ sistema/modificarProducto.php
@@ -3,15 +3,28 @@
     //require 'config/config.php';
     require 'funciones/conexion.php';
     require 'funciones/productos.php';
-
+        $chequeo = modificarProducto();
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
 
     <main class="container">
         <h1>Modificación de datos de un producto</h1>
-
-        reporte
+<?php
+        $class = 'danger';
+        $mensaje = 'No se pudo modificar el producto';
+        if( $chequeo ) {
+            $class = 'success';
+            $mensaje = 'Producto modificado correctamente';
+        }
+?>
+        <div class="alert alert-<?= $class ?>">
+            <?= $mensaje ?>
+            <br>
+            <a href="adminProductos.php" class="btn btn-outline-secondary">
+                volver a panel
+            </a>
+        </div>
 
     </main>
 
