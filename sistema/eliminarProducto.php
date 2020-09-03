0b2336b4ab6fc01a7e3519cd0f7ff91738e8e342
marcos pinardi <exegeses@gmail.com>
2020-09-03 10:55:20 -0300

    aplicación de baja de un producto

---
+++ sistema/eliminarProducto.php
@@ -0,0 +1,31 @@
+<?php  
+
+    //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+        $chequeo = eliminarProducto();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Baja de un producto</h1>
+<?php
+        $class = 'danger';
+        $mensaje = 'No se pudo eliminar el producto';
+        if( $chequeo ) {
+            $class = 'success';
+            $mensaje = 'Producto eliminado correctamente';
+        }
+?>
+        <div class="alert alert-<?= $class ?>">
+            <?= $mensaje ?>
+            <br>
+            <a href="adminProductos.php" class="btn btn-outline-secondary">
+                volver a panel
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
