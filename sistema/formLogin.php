ef109638a25fbc451061e42b579157ae3cc94c2c
marcos pinardi <exegeses@gmail.com>
2020-09-08 12:16:13 -0300

    login con mensaje de error

---
+++ sistema/formLogin.php
@@ -0,0 +1,39 @@
+<?php  
+
+    //require 'config/config.php';
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Ingreso a sistema</h1>
+
+        <div class="alert bg-light p-4 border col-8 mx-auto">
+            <form action="login.php" method="post">
+                Usuario: <br>
+                <input type="text" name="usuEmail" class="form-control">
+                <br>
+                Contraseña: <br>
+                <input type="password" name="usuPass" class="form-control">
+                <br>
+                <button class="btn btn-dark btn-block py-2">
+                    Ingresar
+                </button>
+            </form>
+        </div>
+
+<?php
+        if( isset($_GET['error']) ){
+            //capturamos error
+            //$error = $_GET['error'];
+?>
+        <div class="alert alert-danger col-8 mx-auto">
+            Nombre de usuario y/o contraseña incorrectos.
+        </div>
+<?php
+        }
+?>
+            
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
---
+++ sistema/funciones/autenticar.php
@@ -0,0 +1,40 @@
+<?php
+
+    ########################################
+    #### AUTH: rutinas a autenticación
+    ########################################
+
+    function login()
+    {
+        $usuEmail = $_POST['usuEmail'];
+        $usuPass = $_POST['usuPass'];
+        $link = conectar();
+
+        $sql = "SELECT usuNombre, usuApellido
+                    FROM usuarios
+                    WHERE usuEmail = '".$usuEmail."'
+                      AND usuPass = '".$usuPass."'";
+
+        $resultado = mysqli_query( $link, $sql )
+                        or die(mysqli_error($link));
+        $cantidad = mysqli_num_rows($resultado);
+
+        if( $cantidad == 0 ){
+            //redirección a formLogin
+            header('location: formLogin.php?error=1');
+        }
+        else{
+            // token de autenticación
+
+            //redirección a admin
+            header('location: admin.php');
+        }
+    }
+    function logout()
+    {
+
+    }
+    function autenticar()
+    {
+
+    }
